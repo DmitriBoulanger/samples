2f14151d5471861979eb3f2894ddfa20287ca870
Dmitri Boulanger <dmitri.boulanger@t-online.de>
2014-02-20 00:04:33 +0100

    some comments

--- maven/_index/BasicUseCases/07_Mojo/07_Mojo-Timestamp/src/main/java/de/dbo/samples/maven/mojo/TimestampMojo.java
+++ maven/_index/BasicUseCases/07_Mojo/07_Mojo-Timestamp/src/main/java/de/dbo/samples/maven/mojo/TimestampMojo.java
@@ -6,22 +6,27 @@ import java.util.*;
 import org.apache.maven.plugin.AbstractMojo;
 
 /**
+ * Print out time-stamp and elapsed ms.
  * @goal timestamp
  */
-public class TimestampMojo extends AbstractMojo
+//see: http://books.sonatype.com/mvnref-book/reference/writing-plugins-sect-custom-plugin.html#writing-plugins-sect-class-annotations
+public final class TimestampMojo extends AbstractMojo
 {
+	
    /** @parameter */ String prefix = "Timestamp:";
    /** @parameter */ String datetimePattern = "yyyy-MM-dd HH:mm:ss,S";
 
  @SuppressWarnings("unchecked")
-public void execute() {
+ @Override
+public final void execute() {
       final String CTX_TIME_KEY = "TimestampMojo-Time";
       final Date  date = new Date();
       final Long  timeZuletzt = (Long) getPluginContext().get( CTX_TIME_KEY );
       getPluginContext().put( CTX_TIME_KEY, new Long( date.getTime() ) );
       final String elapsed = ( timeZuletzt == null ) ? "" :
-            ", Elapsed: " + (date.getTime() - timeZuletzt.longValue()) + " ms";
+            " Elapsed: " + (date.getTime() - timeZuletzt.longValue()) + " ms.";
       getLog().info( prefix + " " +
             (new SimpleDateFormat( datetimePattern ).format( date )) + elapsed );
    }
+
 }
