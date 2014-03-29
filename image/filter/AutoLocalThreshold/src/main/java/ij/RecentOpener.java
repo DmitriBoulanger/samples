4642a88fee4e07a1153fbc63984f3ed3f1d67db3
Dmitri Boulanger <dmitri.boulanger@t-online.de>
2014-03-29 11:45:57 +0100

    image-filter ij loaded really

---
+++ image/filter/AutoLocalThreshold/src/main/java/ij/RecentOpener.java
@@ -0,0 +1,38 @@
+package ij;
+import ij.io.Opener;
+
+import java.awt.Menu;
+import java.awt.MenuItem;
+
+/** Opens, in a separate thread, files selected from the File/Open Recent submenu.*/
+public class RecentOpener implements Runnable {
+	private String path;
+
+	RecentOpener(String path) {
+		this.path = path;
+		Thread thread = new Thread(this, "RecentOpener");
+		thread.start();
+	}
+
+	/** Open the file and move the path to top of the submenu. */
+	public void run() {
+		Opener o = new Opener();
+		o.open(path);
+		Menu menu = Menus.getOpenRecentMenu();
+		int n = menu.getItemCount();
+		int index = 0;
+		for (int i=0; i<n; i++) {
+			if (menu.getItem(i).getLabel().equals(path)) {
+				index = i;
+				break;
+			}
+		}
+		if (index>0) {
+			MenuItem item = menu.getItem(index);
+			menu.remove(index);
+			menu.insert(item, 0);
+		}
+	}
+
+}
+
---
+++ image/filter/AutoLocalThreshold/src/main/java/ij/plugin/MeasurementsWriter.java
@@ -0,0 +1,58 @@
+package ij.plugin;
+import ij.IJ;
+import ij.Prefs;
+import ij.WindowManager;
+import ij.io.SaveDialog;
+import ij.measure.ResultsTable;
+import ij.text.TextPanel;
+import ij.text.TextWindow;
+
+import java.awt.Frame;
+import java.io.IOException;
+
+/** Writes measurements to a csv or tab-delimited text file. */
+public class MeasurementsWriter implements PlugIn {
+
+	public void run(String path) {
+		save(path);
+	}
+	
+	public boolean save(String path) {
+		Frame frame = WindowManager.getFrontWindow();
+		if (frame!=null && (frame instanceof TextWindow)) {
+			TextWindow tw = (TextWindow)frame;
+			return tw.getTextPanel().saveAs(path);
+		} else if (IJ.isResultsWindow()) {
+			TextPanel tp = IJ.getTextPanel();
+			if (tp!=null) {
+				if (!tp.saveAs(path))
+					return false;
+			}
+		} else {
+			ResultsTable rt = ResultsTable.getResultsTable();
+			if (rt==null || rt.getCounter()==0) {
+				frame = WindowManager.getFrame("Results");
+				if (frame==null || !(frame instanceof TextWindow))
+					return false;
+				else {
+					TextWindow tw = (TextWindow)frame;
+					return tw.getTextPanel().saveAs(path);
+				}
+			}
+			if (path.equals("")) {
+				SaveDialog sd = new SaveDialog("Save as Text", "Results", Prefs.get("options.ext", ".xls"));
+				String file = sd.getFileName();
+				if (file == null) return false;
+				path = sd.getDirectory() + file;
+			}
+			try {
+				rt.saveAs(path);
+			} catch (IOException e) {
+				IJ.error(""+e);
+			}
+		}
+		return true;
+	}
+
+}
+
