c5dd0e3be6a2341cb57112a0c23a9c7da318cc3a
Dmitri Boulanger <dmitri.boulanger@t-online.de>
2014-03-30 13:25:50 +0200

    Usung imagej-maven src-147 OK

--- image/filter/AutoLocalThreshold/src/main/java/de/dbo/samples/image/filter/ImageJMain.java
+++ image/filter/AutoLocalThreshold/src/main/java/de/dbo/samples/image/filter/ImageJMain.java
@@ -5,10 +5,12 @@ import ij.ImageJ;
 public class ImageJMain {
 	public static final void main(final String[] args) {
 //		ImageJ.main(new String[]{"-debug"});
-		ImageJ.main(new String[]{"-debug",
-		   "-ijpath D:/JAVA/WORKSPACES/ws/samples.git/samples/image/filter/AutoLocalThreshold/src/main/resources/plugins"
+		ImageJ.main(new String[]{
+		   "-port0"
+//		   ,"-debug"
+		   ,"-ijpath D:/JAVA/WORKSPACES/ws/samples.git/samples/image/filter/AutoLocalThreshold/src/main/resources"
 		});
 				
-				ImageJ.main(args);
+		
 	}
 }
