4bcd91e007c1830776fea5d39308e1af91203231
Dmitri Boulanger <dmitri.boulanger@t-online.de>
2014-04-23 18:33:47 +0200

    version done The second ref-implementaion. It is better ...

---
+++ gui/swing/treetable/treetable-impl/src/main/java/de/dbo/samples/gui/swing/treetable/impl/ref/TreetableModelImpl2.java
@@ -0,0 +1,122 @@
+package de.dbo.samples.gui.swing.treetable.impl.ref;
+ 
+import de.dbo.samples.gui.swing.treetable.api.gui.TreetableException;
+import de.dbo.samples.gui.swing.treetable.api.gui.TreetableModel;
+import de.dbo.samples.gui.swing.treetable.api.gui.TreetableModelAbstraction;
+import de.dbo.samples.gui.swing.treetable.api.records.Node;
+import de.dbo.samples.gui.swing.treetable.api.records.Record;
+
+
+import de.dbo.samples.gui.swing.treetable.api.records.RecordRelativeTimestamp;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class TreetableModelImpl2 extends TreetableModelAbstraction {
+	private static final Logger log = LoggerFactory.getLogger(TreetableModelImpl.class);
+	
+    // Names of the columns
+    private static  String[] columnNames = {"Path", "NR", "Timestamp", "Record" };
+ 
+    // Types of the columns
+    private static Class<?>[] columnTypes = { 
+    	TreetableModel.class, Long.class, String.class, Object.class};
+    
+    /**
+     * 
+     * @param root complete data-structure 
+     */
+    public TreetableModelImpl2() {
+        super();
+    }
+    
+    @Override
+    public boolean isCellEditable(Object node, int column) {
+    	 switch (column) {
+         case 0:
+        	 return true; // Important to activate the TreeExpandListener
+         case 1:
+         case 2:
+        	 return false; // sequence/timestamp-attributes are immutable
+         case 3:
+        	 return true;
+        	 
+         default:
+             throw new TreetableException(
+                "Incorrect column in isCellEditable(Object node="+node.toString()+", int column="+column+")");
+         }
+    }
+    
+    @Override
+    public Object getValueAt(Object node, int column) {
+    	log.trace("setValueAt(Object node="+node.toString()+", int column="+column+")");
+        switch (column) {
+        case 0:
+            return ((Node) node).getTreename();
+            
+        case 1:
+        	return ((Node) node).getSequence();
+        	
+        case 2:
+        	final Record record = (Record) ((Node) node).getContents();
+        	final RecordRelativeTimestamp timestamp;
+        	if (null!=record) {
+				timestamp = record.relativeTimestamp();
+			} else {
+				timestamp =  RecordRelativeTimestamp.TIMESTAMP_NULL;
+			}
+        	return timestamp.print(RecordRelativeTimestamp.FORMAT_COMPRESSED);
+        	
+		case 3:
+       	     return ((Node) node).getContents();
+            
+        default:
+            throw new TreetableException(
+            	"Incorrect column in getValueAt(Object node="+node.toString()+", int column="+column+")");
+        } 
+    }
+   
+    @Override
+    public void setValueAt(Object value, Object node, int column) {
+    	 switch (column) {
+         case 0:
+         case 1:
+        	 log.error("setValueAt(Object value="+value+", Object node="+node.toString()+", int column="+column+") rejected");
+        	 break;
+        	 
+         case 3:
+        	 log.trace("setValueAt(Object value="+value+", Object node="+node.toString()+", int column="+column+")");
+        	 ((Node) node).setContents(value);
+        	 break;
+         
+         default:
+             throw new TreetableException(
+                 	"Incorrect column in setValueAt(Object value="+value+", Object node="+node.toString()+", int column="+column+")");
+         }
+    }
+    
+    @Override
+    public Object getChild(Object parent, int index) {
+        return ((Node) parent).getChildren().get(index);
+    }
+ 
+    @Override
+    public int getChildCount(Object parent) {
+        return ((Node) parent).getChildren().size();
+    }
+ 
+    @Override
+    public int getColumnCount() {
+        return columnNames.length;
+    }
+ 
+    @Override
+    public String getColumnName(int column) {
+        return columnNames[column];
+    }
+ 
+    @Override
+    public Class<?> getColumnClass(int column) {
+        return columnTypes[column];
+    }
+}
