f69d7f48a653894fd97d44f619a38eb3bfd60dcb
Dmitri Boulanger <dmitri.boulanger@t-online.de>
2014-04-27 00:41:28 +0200

    missing files

---
+++ gui/swing/treetable/treetable-api/src/main/java/de/dbo/samples/gui/swing/treetable/api/xgui/TreeIcon.java
@@ -0,0 +1,32 @@
+package de.dbo.samples.gui.swing.treetable.api.xgui;
+
+import de.dbo.samples.gui.swing.treetable.api.records.Node;
+
+import javax.swing.Icon;
+import javax.swing.UIManager;
+
+import org.jdesktop.swingx.renderer.IconValue;
+import org.jdesktop.swingx.icon.EmptyIcon;
+
+public class TreeIcon implements IconValue {
+	private static final long serialVersionUID = 8218192615206835643L;
+
+	private static final Icon NODE_ICON = UIManager.getIcon("FileView.directoryIcon");
+	private static final Icon LEAF_ICON = new EmptyIcon();
+
+	@Override
+	public Icon getIcon(Object node) {
+		if (null==node) {
+			return null;
+		}
+		 
+			if ( ((Node) node).getChildren().isEmpty() ){
+				return LEAF_ICON;
+			}
+			return NODE_ICON;
+		 
+		
+	}
+ 
+
+}
