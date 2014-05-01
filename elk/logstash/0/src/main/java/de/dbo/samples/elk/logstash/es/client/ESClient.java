1495b158eb99872ab6efd48cb486a47abecaeb41
Dmitri Boulanger <dmitri.boulanger@t-online.de>
2014-05-02 00:12:52 +0200

    elk logstash ESClient test

--- elk/logstash/0/src/main/java/de/dbo/samples/elk/logstash/es/client/ESClient.java
+++ elk/logstash/0/src/main/java/de/dbo/samples/elk/logstash/es/client/ESClient.java
@@ -1,6 +1,7 @@
 package de.dbo.samples.elk.logstash.es.client;
  
 
+import java.util.Date;
 import java.util.Map;
 
 import org.elasticsearch.action.admin.indices.mapping.put.PutMappingRequestBuilder;
@@ -13,6 +14,8 @@ import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.joda.time.DateTime;
 import org.elasticsearch.common.joda.time.format.DateTimeFormat;
 import org.elasticsearch.common.joda.time.format.DateTimeFormatter;
+import org.elasticsearch.common.settings.ImmutableSettings;
+import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
 import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.common.xcontent.XContentBuilder;
@@ -33,69 +36,67 @@ import static org.elasticsearch.index.query.QueryBuilders.matchAllQuery;
 import static org.elasticsearch.node.NodeBuilder.nodeBuilder;
 
 public class ESClient {
-    final static Logger logger = LoggerFactory.getLogger(ESClient.class);
-    public static void main(String[] args) {
-//          Node node = null;
-          Client client = null;
-          
-         logger.info("Initializing Search Server - Bootstrap ...");
-//         node = nodeBuilder().node();
-          
-         client = new TransportClient() .addTransportAddress(new
-        			InetSocketTransportAddress("127.0.0.1", 9300));
-         logger.info("Initializing Search Server - Bootstrap - DONE");
-         
-//         if (true) return;
-
-//        XContentBuilder indexSettings = null;
-
-//        FilterBuilder typeFilter = FilterBuilders.andFilter(
- 
-//        FilterBuilders.termsFilter("classification","schedule"),
-//                       FilterBuilders.termsFilter("type","outlookcalendar") ).filterName("classify");
-//
-
-       
-        DateTime dt1 =  DateTime.now().minus(1000*60*15);
-        DateTime dt2 =  DateTime.now(); 
-        RangeFilterBuilder rangeFilterBuilder = FilterBuilders.rangeFilter("@timestamp");
-        rangeFilterBuilder.from(dt1).to(dt2);
-//
-//        AndFilterBuilder queryFilters = FilterBuilders.andFilter();
-//                queryFilters.add(FilterBuilders.termFilter("classification","scehdule"));
-//        queryFilters.add(FilterBuilders.termFilter("type","excalendar"));
-//
-//        
-
-//        FilterBuilder aggFilter = FilterBuilders.andFilter(queryFilters,rangeFilterBuilder);
-        FilterBuilder aggFilter = FilterBuilders.andFilter(rangeFilterBuilder);
-
-//        QueryBuilder queryBuilder = QueryBuilders.filteredQuery(QueryBuilders.queryString("coffee"),
-//                                           aggFilter);
-        
-        QueryBuilder queryBuilder = QueryBuilders.filteredQuery(QueryBuilders.queryString("*"),
-                aggFilter);
-
-        logger.info("running ...");
-        SearchResponse response = client.prepareSearch("logstash-2014.03.23")
-                                                   .setSearchType(SearchType.QUERY_AND_FETCH)
-                                                   .setQuery(queryBuilder)
-                                        
-//                                                   .setFilter(aggFilter)
-//                                                   .setFrom(0).setSize(50).setExplain(true)
-//                                                   .setScroll(new TimeValue(600000))
-                                                   .setExplain(false)
-                                                   .execute()
-                                                   .actionGet();
-
-//   SearchHits hits = response.getHits();
-//   for (SearchHit hit : hits.getHits() ) {
-//	   Map<String, ?> map =  hit
-//	   System.out.println(map);
-//   }
-      
-
-        System.out.println(response);
-    } 
+	private final static Logger log = LoggerFactory.getLogger(ESClient.class);
+	
+	private final Settings settings;
+	private final InetSocketTransportAddress inetSocketTransportAddress;
+	
+	private TransportClient client = null;
+	 
+	public ESClient() {
+		settings = ImmutableSettings.settingsBuilder()
+		        .put("cluster.name", "elasticsearch-hombach")
+		        .build();
+		inetSocketTransportAddress = 
+				new InetSocketTransportAddress("127.0.0.1", 9300);
+		log.info("settings and server address set");
+	}
+	
+	public final void open() {
+		if (null==client) {
+			client = new TransportClient(settings); 
+			client.addTransportAddress(inetSocketTransportAddress);
+			log.info("opened");
+		} 
+	}
+	
+	public void close() {
+		if (null!=client) {
+			client.close(); 
+			client = null;
+			log.info("closed");
+		} 
+	}
+	
+	public QueryBuilder query() {
+	     return QueryBuilders.matchAllQuery();
+	}
+	
+	public RangeFilterBuilder range(final int minFromNow) {
+		 final DateTime dt1 =  DateTime.now().minus(1000*60*minFromNow);
+	     final DateTime dt2 =  DateTime.now(); 
+	     log.info("range from=" + new Date(dt1.getMillis())  + " to=" + new Date(dt2.getMillis()) );
+	     return FilterBuilders.rangeFilter("@timestamp").from(dt1).to(dt2);
+	}
+	
+	public void run(final QueryBuilder queryBuilder) {
+		if (null==client) {
+			log.warn("can't run query: no client opend");
+			return;
+		}
+		log.info("query ... ");
+		final SearchResponse response = client.prepareSearch("logstash-2014.03.23")
+                .setSearchType(SearchType.QUERY_AND_FETCH)
+                .setQuery(queryBuilder)
+                .setExplain(false)
+                .execute()
+                .actionGet();
+		final SearchHit[] searchHits = response.getHits().getHits();
+		log.info("query hits: " + searchHits.length);
+		for (final SearchHit searchHit :searchHits) {
+			log.info(searchHit.getId());
+		}
+		
+	}
     
 }
--- elk/logstash/0/src/test/java/de/dbo/samples/elk/logstash/ESClientTest.java
+++ elk/logstash/0/src/test/java/de/dbo/samples/elk/logstash/ESClientTest.java
@@ -2,90 +2,19 @@ package de.dbo.samples.elk.logstash;
 
 import de.dbo.samples.elk.logstash.es.client.ESClient;
 
-import org.elasticsearch.action.search.SearchResponse;
-import org.elasticsearch.action.search.SearchType;
-import org.elasticsearch.client.Client;
-import org.elasticsearch.client.transport.TransportClient;
-import org.elasticsearch.common.joda.time.DateTime;
-import org.elasticsearch.common.settings.ImmutableSettings;
-import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.common.transport.InetSocketTransportAddress;
-import org.elasticsearch.index.query.FilterBuilder;
-import org.elasticsearch.index.query.FilterBuilders;
 import org.elasticsearch.index.query.QueryBuilder;
-import org.elasticsearch.index.query.QueryBuilders;
-import org.elasticsearch.index.query.RangeFilterBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ESClientTest {
-	   final static Logger logger = LoggerFactory.getLogger(ESClient.class);
+	   final static Logger logger = LoggerFactory.getLogger(ESClientTest.class);
 	   
 	    public static void main(String[] args) {
-	          Client client = null;
+	    	final ESClient esClient = new ESClient();
+	    	final QueryBuilder query = esClient.query();
+	    	esClient.open();
+	    	esClient.run(query);
+	    	esClient.close();
+	    }
 	          
-	          final Settings settings = ImmutableSettings.settingsBuilder()
-	        	        .put("cluster.name", "elasticsearch-hombach")
-	        	        .build();
-	         logger.info("cluster.name = " + settings.get("cluster.name"));
-
-	         logger.info("Initializing Search Server - Bootstrap ...");
-	         client = new TransportClient(settings) .addTransportAddress(new
-	        			InetSocketTransportAddress("127.0.0.1", 9300));
-	         logger.info("Initializing Search Server - Bootstrap - DONE");
-	         
-//	         if (true) return;
-
-//	        XContentBuilder indexSettings = null;
-
-//	        FilterBuilder typeFilter = FilterBuilders.andFilter(
-	 
-//	        FilterBuilders.termsFilter("classification","schedule"),
-//	                       FilterBuilders.termsFilter("type","outlookcalendar") ).filterName("classify");
-	//
-
-	       
-	        DateTime dt1 =  DateTime.now().minus(1000*60*15);
-	        DateTime dt2 =  DateTime.now(); 
-	        RangeFilterBuilder rangeFilterBuilder = FilterBuilders.rangeFilter("@timestamp");
-	        rangeFilterBuilder.from(dt1).to(dt2);
-	//
-//	        AndFilterBuilder queryFilters = FilterBuilders.andFilter();
-//	                queryFilters.add(FilterBuilders.termFilter("classification","scehdule"));
-//	        queryFilters.add(FilterBuilders.termFilter("type","excalendar"));
-	//
-//	        
-
-//	        FilterBuilder aggFilter = FilterBuilders.andFilter(queryFilters,rangeFilterBuilder);
-	        FilterBuilder aggFilter = FilterBuilders.andFilter(rangeFilterBuilder);
-
-//	        QueryBuilder queryBuilder = QueryBuilders.filteredQuery(QueryBuilders.queryString("coffee"),
-//	                                           aggFilter);
-	        
-	        QueryBuilder queryBuilder = QueryBuilders.filteredQuery(QueryBuilders.queryString("*"),
-	                aggFilter);
-
-	        logger.info("running ...");
-	        SearchResponse response = 
-	        		client.prepareSearch("logstash-2014.05.01")
-	                .setSearchType(SearchType.QUERY_AND_FETCH)
-	                .setQuery(queryBuilder)
-	                                        
-//	                                                   .setFilter(aggFilter)
-//	                                                   .setFrom(0).setSize(50).setExplain(true)
-//	                                                   .setScroll(new TimeValue(600000))
-	                 .setExplain(false) /* no ranking */
-	                 .execute()
-	                 .actionGet();
-	        client.close();
-
-	//   SearchHits hits = response.getHits();
-	//   for (SearchHit hit : hits.getHits() ) {
-//		   Map<String, ?> map =  hit
-//		   System.out.println(map);
-	//   }
-	      
-
-	        System.out.println(response);
-	    } 
 }
