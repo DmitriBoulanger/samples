43e04682a443c58d3986b8052d85851744f1284b
Dmitri Boulanger <dmitri.boulanger@t-online.de>
2014-05-04 15:25:47 +0200

    refactor

---
+++ elk/logstash/logstash-0/src/scripts/es/logstash-es-localhost-log4jappender.bat
@@ -0,0 +1,2 @@
+
+java -jar ..\logstash-1.3.3-flatjar.jar agent -f logstash-es-localhost-log4jappender.conf -- web
---
+++ elk/logstash/logstash-0/src/scripts/standalone/logstash-embedded-logback.bat
@@ -0,0 +1,2 @@
+
+java -jar ..\logstash-1.3.3-flatjar.jar agent -f logstash-embedded-logback.conf -- web
