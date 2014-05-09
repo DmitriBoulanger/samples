65f8ae85fa5adc088203f75e5584c5e1dd65a0b6
boulanger <boulanger@ityx.de>
2014-05-09 16:41:34 +0200

    make-up

--- elk/logstash/logstash-0/src/test/java/de/dbo/samples/elk/logstash/client0/ApplicationMessages.java
+++ elk/logstash/logstash-0/src/test/java/de/dbo/samples/elk/logstash/client0/ApplicationMessages.java
@@ -2,22 +2,23 @@ package de.dbo.samples.elk.logstash.client0;
 
 import java.util.UUID;
 
-import org.slf4j.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Application messages.
  * Messages are generated using SLF4J-Loggers with different names.
  * Actual logger-resource is Log4J that is configured in log4j.properties
- * 
+ *
  * @author Dmitri Boulanger, Hombach
  *
- * D. Knuth: Programs are meant to be read by humans and 
- *           only incidentally for computers to execute 
+ * D. Knuth: Programs are meant to be read by humans and
+ *           only incidentally for computers to execute
  *
  */
 public final class ApplicationMessages {
 	private static final Logger log = LoggerFactory.getLogger(ApplicationMessages.class);
-	
+
 	public static final Logger LOGGER1 = LoggerFactory.getLogger("AnotherLogger");
 	public static final Logger LOGGER2 = LoggerFactory.getLogger("AnotherLoggerX");
 
