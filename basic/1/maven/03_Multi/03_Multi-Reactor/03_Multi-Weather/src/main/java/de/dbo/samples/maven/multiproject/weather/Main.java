79e25d55e1243971c42b57137184891195f9b215
Dmitri Boulanger <dmitri.boulanger@t-online.de>
2014-06-10 23:00:40 +0200

    xmind poms make-up, optimization a bit

--- basic/1/maven/03_Multi/03_Multi-Reactor/03_Multi-Weather/src/main/java/de/dbo/samples/maven/multiproject/weather/Main.java
+++ basic/1/maven/03_Multi/03_Multi-Reactor/03_Multi-Weather/src/main/java/de/dbo/samples/maven/multiproject/weather/Main.java
@@ -2,14 +2,14 @@ package de.dbo.samples.maven.multiproject.weather;
 
 import java.io.InputStream;
 
-import org.apache.log4j.PropertyConfigurator;
+//import org.apache.log4j.PropertyConfigurator;
 
 
 public class Main {
 
 	public static void main(String[] args) throws Exception {
 		// Configure Log4J
-		PropertyConfigurator.configure(Main.class.getClassLoader().getResource("log4j.properties"));
+		// PropertyConfigurator.configure(Main.class.getClassLoader().getResource("log4j.properties"));
 
 		// Read the Zip Code from the Command-line (if none supplied, use 60202)
 		String zipcode = "60202";
