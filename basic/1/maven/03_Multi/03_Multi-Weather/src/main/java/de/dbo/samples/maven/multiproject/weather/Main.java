4a6b1c0a84ec7018de61fb7ceb460a4140b0632d
Dmitri Boulanger <dmitri.boulanger@t-online.de>
2014-06-14 00:43:40 +0200

    Logger resource for basic1

--- basic/1/maven/03_Multi/03_Multi-Weather/src/main/java/de/dbo/samples/maven/multiproject/weather/Main.java
+++ basic/1/maven/03_Multi/03_Multi-Weather/src/main/java/de/dbo/samples/maven/multiproject/weather/Main.java
@@ -2,15 +2,9 @@ package de.dbo.samples.maven.multiproject.weather;
 
 import java.io.InputStream;
 
-//import org.apache.log4j.PropertyConfigurator;
-
-
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-		// Configure Log4J
-		// PropertyConfigurator.configure(Main.class.getClassLoader().getResource("log4j.properties"));
-
 		// Read the Zip Code from the Command-line (if none supplied, use 60202)
 		String zipcode = "60202";
         try {
