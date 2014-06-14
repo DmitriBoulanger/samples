03b37fbda08c292f83933c1eed57f1d911359cd5
Dmitri Boulanger <dmitri.boulanger@t-online.de>
2014-06-14 11:38:57 +0200

    _logger as resource in basic1.maven

--- basic/1/maven/02_Web/src/main/java/de/dbo/samples/basic1/maven/SimpleServlet.java
+++ basic/1/maven/02_Web/src/main/java/de/dbo/samples/basic1/maven/SimpleServlet.java
@@ -1,5 +1,7 @@
 package de.dbo.samples.basic1.maven;
 
+import static de.dbo.samples.util.print.Profiler.elapsed;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -8,14 +10,22 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class SimpleServlet extends HttpServlet {
 	private static final long serialVersionUID = -7032592647920933370L;
+	private static final Logger log = LoggerFactory.getLogger(SimpleServlet.class);
 
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
-        throws ServletException, IOException {
-	PrintWriter out = response.getWriter();
-	out.println("doGet(HttpServletRequest request, HttpServletResponse response): SimpleServlet from Samples Executed" );
-        out.flush();
-        out.close();
-    }
+			throws ServletException, IOException {
+		final long start = System.currentTimeMillis();
+		PrintWriter out = response.getWriter();
+		out.println("doGet(HttpServletRequest request, HttpServletResponse response): SimpleServlet from Samples Executed");
+		out.flush();
+		out.close();
+		out = null;
+		log.info("doGet-request done." + elapsed(start));
+	}
+	
 }
