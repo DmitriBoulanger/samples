0c2a9965e88c501480e685192e67795093b520e6
Dmitri Boulanger <dmitri.boulanger@t-online.de>
2014-06-15 17:44:15 +0200

    big refactoring agian and again ... ... ... make-up

--- basic/1/maven/00_Simple/01_Custom/src/main/java/de/dbo/samples/basic1/maven/simple/WeatherFormatter.java
+++ basic/1/maven/00_Simple/01_Custom/src/main/java/de/dbo/samples/basic1/maven/simple/WeatherFormatter.java
@@ -4,9 +4,10 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 import java.io.StringWriter;
 
-import org.slf4j.*;
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.Velocity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class WeatherFormatter {
 
