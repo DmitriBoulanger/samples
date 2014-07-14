3fd4c620438d6c9c18d60acb2d881a96b0551363
Dmitri Boulanger <boulanger@ityx.de>
2014-07-14 19:03:48 +0200

    some corrections in POMs ...

--- bdd/cucumber/cucumber-master/cucumber-master-cdi/src/test/java/de/dbo/samples/cucumber/master/cdi/CDICucumberScenario.java
+++ bdd/cucumber/cucumber-master/cucumber-master-cdi/src/test/java/de/dbo/samples/cucumber/master/cdi/CDICucumberScenario.java
@@ -1,8 +1,9 @@
 package de.dbo.samples.cucumber.master.cdi;
 
-import de.dbo.samples.cucumber.master.project.ATM;
-import de.dbo.samples.cucumber.master.project.Account;
-import de.dbo.samples.cucumber.master.project.CreditCard;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
@@ -11,11 +12,9 @@ import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertThat;
+import de.dbo.samples.cucumber.master.project.ATM;
+import de.dbo.samples.cucumber.master.project.Account;
+import de.dbo.samples.cucumber.master.project.CreditCard;
 
 
 @Singleton
@@ -23,41 +22,42 @@ public class CDICucumberScenario {
 	@Inject private ATM atm;
 	@Inject private CreditCard creditCard;
 	@Inject private Account account;
+
 	private int money;
-	
+
 	@Given("^the account balance is (\\d*)$")
     public void createAccount(int balance) {
 		//assertNotNull(account);
         account.setBalance(balance);
-    } 
-	
+    }
+
 	@And("^the card is valid$")
     public void createCreditCard() {
 		assertNotNull(creditCard);
 		creditCard.setAccount(account);
-    } 
-	
+    }
+
 	@And("^the machine contains (\\d*)$")
     public void createATM(int money) {
 		assertNotNull(atm);
 		atm.setMoney(money);
-    } 
-	
+    }
+
     @When("^the Account Holder requests (\\d*)$")
     public void requestMoney(int amount) {
         money = atm.requestMoney(creditCard, amount);
     }
-    
+
     @Then("^the ATM should dispense (\\d*)$")
     public void checkMoney(int amount) {
         assertThat(money, is(amount));
     }
-    
+
     @And("^the account balance should be (\\d*)$")
     public void checkBalance(int newBalance) {
         assertThat(newBalance, is(creditCard.getAccount().getBalance()));
     }
-    
+
     @And("^the card should be returned$")
     public void cardShouldBeReturned() {
         assertFalse(creditCard.isInUse());
--- bdd/cucumber/cucumber-simple/src/test/java/de/dbo/samples/cucumber/simple/DepositStepDefinitions.java
+++ bdd/cucumber/cucumber-simple/src/test/java/de/dbo/samples/cucumber/simple/DepositStepDefinitions.java
@@ -1,42 +1,42 @@
 package de.dbo.samples.cucumber.simple;
 
+import static org.junit.Assert.assertTrue;
+import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
-import static org.junit.Assert.assertTrue;
-
 public class DepositStepDefinitions {
- 
+
 	private User user;
     private Account account;
- 
-    @cucumber.api.java.Before(value="@UserAccount")
+
+    @Before(value = "@UserAccount")
     public void initialize() {
         if (user == null) {
             user = new User();
         }
- 
+
         if (account == null) {
             account = new Account();
             user.setAccount(account);
         }
     }
-    
+
     @Given("^a User has no money in their account$")
     public void a_User_has_no_money_in_their_current_account() {
         assertTrue("The balance is not zero.", account.getBalance() == 0L);
     }
- 
+
     @When("^€(\\d+) is deposited in to the account$")
     public void £_is_deposited_in_to_the_account(int amount) {
         account.deposit(amount);
     }
- 
+
     @Then("^the balance should be €(\\d+)$")
     public void the_balance_should_be_£(int expectedBalance) {
         int currentBalance = account.getBalance();
         assertTrue("The expected balance was €100, but actually was: " + currentBalance, currentBalance == expectedBalance);
     }
- 
+
 }
