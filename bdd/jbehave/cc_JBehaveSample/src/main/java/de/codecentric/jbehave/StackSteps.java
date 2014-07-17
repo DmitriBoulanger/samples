53c294a532f6b9b1d9f15b7688fa324dad82a2dd
Dmitri Boulanger <boulanger@ityx.de>
2014-07-17 19:24:17 +0200

    small chenages ....

--- bdd/jbehave/cc_JBehaveSample/src/main/java/de/codecentric/jbehave/StackSteps.java
+++ bdd/jbehave/cc_JBehaveSample/src/main/java/de/codecentric/jbehave/StackSteps.java
@@ -1,25 +1,29 @@
 package de.codecentric.jbehave;
 
 import java.util.Stack;
-import org.jbehave.core.annotations.*;
-import org.jbehave.core.embedder.Embedder;
+
 import junit.framework.Assert;
+
+import org.jbehave.core.annotations.Given;
+import org.jbehave.core.annotations.Then;
+import org.jbehave.core.annotations.When;
+import org.jbehave.core.embedder.Embedder;
 
 public class StackSteps extends Embedder{
 
     private Stack<String> testStack;
     private String searchElement;
- 
+
     @Given("an empty stack")
     public void anEmptyStack() {
         testStack = new Stack<String>();
     }
-    
+
     @When("the string $element is added")
     public void anElementIsAdded(String element) {
         testStack.push(element);
     }
- 
+
     @When("the last element is removed again")
     public void removeLastElement() {
         testStack.pop();
@@ -29,14 +33,14 @@ public class StackSteps extends Embedder{
     public void searchForElement(String element) {
         searchElement = element;
     }
-    
+
     @Then("the resulting element should be $result")
     public void theResultingElementShouldBe(String result) {
     	Assert.assertEquals(testStack.pop(), result);
     }
-    
-    @Then("the position returned should be $pos")
-    public void thePositionReturnedShouldBe(int pos) {
-    	Assert.assertEquals(testStack.search(searchElement), pos);
-    }
+
+    //    @Then("the position returned should be $pos")
+    //    public void thePositionReturnedShouldBe(int pos) {
+    //    	Assert.assertEquals(testStack.search(searchElement), pos);
+    //    }
 }
