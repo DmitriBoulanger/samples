4c90633dc1993b0ced50f7428cbd2f6e7b6237e6
Dmitri Boulanger <dmitri.boulanger@t-online.de>
2014-09-22 20:36:50 +0200

    Deadlock make-up 2

--- basic/1/concurrent/Semaphore/src/main/java/de/dbo/samples/concurrent/semaphore/Deadlock.java
+++ basic/1/concurrent/Semaphore/src/main/java/de/dbo/samples/concurrent/semaphore/Deadlock.java
@@ -11,7 +11,7 @@ public class Deadlock {
 	private static final Logger log = LoggerFactory.getLogger(Deadlock.class);
 	
 	public static void main(String[] args) throws Exception {
-		   final long duration = 1;
+		   final long duration = 0;
 		   log.info("Job duration is about " + duration + " milliseconds");
 		   final Semaphore s1 = new Semaphore(1);
 		   final Semaphore s2 = new Semaphore(1);
@@ -20,7 +20,7 @@ public class Deadlock {
 		   final Thread t = new Thread(new ResourceWithDoubleGrabber(duration, s1, s2, countDownLatch));
 		   
 		   /*
-		    * now reverse them ... here comes trouble!
+		    * now reverse them ... here comes the trouble!
 		    */
 		   final Thread t2 = new Thread(new ResourceWithDoubleGrabber(duration, s2, s1, countDownLatch));
 
@@ -28,17 +28,16 @@ public class Deadlock {
 		   t.start();
 		   t2.start();
 
-		   final long wait = 3*duration;
+		   final long wait = 3*duration + 100;
 		   final boolean ok = countDownLatch.await(wait, TimeUnit.MILLISECONDS);
 		   if (!ok) {
-			log.error("Deadlock occurs! Has been waiting "+wait+ " milliseconds ...");
-			log.info("Exit ...");
-			System.exit(-1);
-			return;
+			   log.error("Deadlock occurs! Has been waiting "+ wait + " milliseconds ...");
+			   log.info("Exit ...");
+			   System.exit(-1);
+			   return;
 		   } else {
 			  log.info("We got lucky!");
 		   }
-		   
 		}
 
 }
