2bc68eb64d5d99ca0b011c3f0f04031ea52da410
Dmitri Boulanger <dmitri.boulanger@t-online.de>
2015-01-24 16:27:01 +0100

    important make-up 2

--- basic/1/spring2/SpringPropertyPlaceholder/src/main/java/de/dbo/samples/spring/properties/main/ServerStartUp.java
+++ basic/1/spring2/SpringPropertyPlaceholder/src/main/java/de/dbo/samples/spring/properties/main/ServerStartUp.java
@@ -5,6 +5,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class ServerStartUp {
 
 	public static void main(String[] args) throws Exception { 
+		Thread.currentThread().setName("RMI-Server start-up");
 		System.setProperty("product.conf","conf");
 		System.setProperty("service.conf.name","From system properties");
 		
