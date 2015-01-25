319af97fcccd1718634208b9b513ba2c700ff9af
Dmitri Boulanger <dmitri.boulanger@t-online.de>
2015-01-25 23:52:15 +0100

    pom-structure correction. Dependencies are standart (from the root)

--- basic/2/spring2/SpringBeanAutowiring/src/test/java/de/dbo/samples/spring/autowiring/SprintAutowireJUnitTest.java
+++ basic/2/spring2/SpringBeanAutowiring/src/test/java/de/dbo/samples/spring/autowiring/SprintAutowireJUnitTest.java
@@ -11,7 +11,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.*;
 import static org.junit.Assert.assertNotNull;
 
 public class SprintAutowireJUnitTest {
