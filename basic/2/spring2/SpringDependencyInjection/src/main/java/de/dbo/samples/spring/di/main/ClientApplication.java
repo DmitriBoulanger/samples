e0796b8a95bb1beb93677d7a3ae10fca8c80af24
Dmitri Boulanger <dmitri.boulanger@t-online.de>
2015-02-01 22:04:57 +0100

    some experiments again and again ...

--- basic/2/spring2/SpringDependencyInjection/src/main/java/de/dbo/samples/spring/di/main/ClientApplication.java
+++ basic/2/spring2/SpringDependencyInjection/src/main/java/de/dbo/samples/spring/di/main/ClientApplication.java
@@ -8,7 +8,8 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class ClientApplication {
 
     public static void main(String[] args) {
-	AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DIConfiguration.class);
+	final AnnotationConfigApplicationContext context = 
+		new AnnotationConfigApplicationContext(DIConfiguration.class);
 	MyApplication app = context.getBean(MyApplication.class);
 
 	app.processMessage("Hi Pankaj", "pankaj@abc.com");
