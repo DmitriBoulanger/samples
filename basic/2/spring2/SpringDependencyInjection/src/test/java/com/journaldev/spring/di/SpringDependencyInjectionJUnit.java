eb2ec77dd837a939c9507682200cc12e3a02ebf6
Dmitri Boulanger <dmitri.boulanger@t-online.de>
2015-02-01 21:17:38 +0100

    springBeans

--- basic/2/spring2/SpringDependencyInjection/src/test/java/com/journaldev/spring/di/SpringDependencyInjectionJUnit.java
+++ basic/2/spring2/SpringDependencyInjection/src/test/java/com/journaldev/spring/di/SpringDependencyInjectionJUnit.java
@@ -13,37 +13,35 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(value="de.dbo.samples.spring.di.consumer")
+@ComponentScan(value = "de.dbo.samples.spring.di.consumer")
 public class SpringDependencyInjectionJUnit {
-	
-	private AnnotationConfigApplicationContext context = null;
 
-	@Bean
-	public MessageService getMessageService() {
-		return new MessageService(){
+    private AnnotationConfigApplicationContext context = null;
 
-			public boolean sendMessage(String msg, String rec) {
-				System.out.println("Mock Service");
-				return true;
-			}
-			
-		};
-	}
+    @Bean
+    public MessageService getMessageService() {
+	return new MessageService() {
+	    public boolean sendMessage(String msg, String rec) {
+		System.out.println("Mock Service");
+		return true;
+	    }
 
-	@Before
-	public void setUp() throws Exception {
-		context = new AnnotationConfigApplicationContext(SpringDependencyInjectionJUnit.class);
-	}
-	
-	@After
-	public void tearDown() throws Exception {
-		context.close();
-	}
+	};
+    }
 
-	@Test
-	public void test() {
-		MyApplication app = context.getBean(MyApplication.class);
-		Assert.assertTrue(app.processMessage("Hi Pankaj", "pankaj@abc.com"));
-	}
+    @Before
+    public void setUp() throws Exception {
+	context = new AnnotationConfigApplicationContext(SpringDependencyInjectionJUnit.class);
+    }
 
+    @After
+    public void tearDown() throws Exception {
+	context.close();
+    }
+
+    @Test
+    public void test() {
+	MyApplication app = context.getBean(MyApplication.class);
+	Assert.assertTrue(app.processMessage("Hi Pankaj", "pankaj@abc.com"));
+    }
 }
