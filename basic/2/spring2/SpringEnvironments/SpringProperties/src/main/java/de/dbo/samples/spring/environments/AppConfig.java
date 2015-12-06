c7cee7acb8742a98791165f4d13c6e8c92722be0
Dmitri Boulanger <Dmitri Boulanger@Hombach-PC>
2015-12-06 22:14:02 +0100

    make-up

--- basic/2/spring2/SpringEnvironments/SpringProperties/src/main/java/de/dbo/samples/spring/environments/AppConfig.java
+++ basic/2/spring2/SpringEnvironments/SpringProperties/src/main/java/de/dbo/samples/spring/environments/AppConfig.java
@@ -5,9 +5,9 @@ import de.dbo.samples.spring.environments.jcg.prop.DatabaseProperties;
 import de.dbo.samples.spring.environments.jcg.prop.JmsProperties;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
+//import org.springframework.core.env.Environment;
 
 @Configuration("classpath:spring/xml-config-context.xml")
 public class AppConfig {
--- basic/2/spring2/SpringEnvironments/SpringProperties/src/test/java/de/dbo/samples/spring/environments/jcg/test/Main.java
+++ basic/2/spring2/SpringEnvironments/SpringProperties/src/test/java/de/dbo/samples/spring/environments/jcg/test/Main.java
@@ -1,6 +1,6 @@
 package de.dbo.samples.spring.environments.jcg.test;
 
-import de.dbo.samples.spring.environments.AppConfig;
+//import de.dbo.samples.spring.environments.AppConfig;
 import de.dbo.samples.spring.environments.jcg.prop.DatabaseProperties;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
