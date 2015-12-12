be3c001fbe58ed7a80ad4ada0e15c343d690dce1
Dmitri Boulanger <Dmitri Boulanger@Hombach-PC>
2015-12-12 13:51:09 +0100

    Global application integration configuration - important extensions 3

--- basic/2/spring2/SpringEnvironments/SpringPropertiesSourceAnnotation/src/main/java/de/dbo/samples/spring/environments/annotation/Application.java
+++ basic/2/spring2/SpringEnvironments/SpringPropertiesSourceAnnotation/src/main/java/de/dbo/samples/spring/environments/annotation/Application.java
@@ -5,6 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class Application {
     
     @Autowired
-    ApplicationConfiguration configuration;
+    private ApplicationConfiguration configuration;
 
+    public ApplicationConfiguration getConfiguration() {
+        return configuration;
+    }
+    
 }
