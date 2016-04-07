2b30662a3c720215ce7bffa5990c93866dfb9126
Dmitri Boulanger <Dmitri Boulanger@Hombach-PC>
2016-04-07 21:46:33 +0200

    important make-up of the JUnit-test

--- mockito/mockito-junit/src/test/java/de/dbo/samples/mockito/ServiceTestServiceMockJUnit.java
+++ mockito/mockito-junit/src/test/java/de/dbo/samples/mockito/ServiceTestServiceMockJUnit.java
@@ -17,86 +17,103 @@ import org.mockito.Mockito;
  */
 public class ServiceTestServiceMockJUnit {
 
-	/**
-	 * Test1.
-	 */
-	@Test
-	public void testSimpleInt() {
-		// create mock
-		TestService test = Mockito.mock(TestService.class);
+    /**
+     * Test 1.
+     */
+    @Test
+    public void test1_SimpleInt() {
+	// create mock
+	TestService serviceMock = Mockito.mock(TestService.class);
 
-		// define return value for method getUniqueId()
-		when(test.getUniqueId()).thenReturn(43);
+	// define return value for method getUniqueId()
+	when(serviceMock.getUniqueId()).thenReturn(43);
 
-		// use mock in test....
-		assertEquals(test.getUniqueId(), 43);
-	}
+	// use mock in test....
+	assertEquals(serviceMock.getUniqueId(), 43);
+    }
 
-	/**
-	 * Test more than one return value.
-	 */
-	// Demonstrates the return of multiple values
-	@Test
-	public void testMoreThanOneReturnValue() {
-		Iterator i = mock(Iterator.class);
-		when(i.next()).thenReturn("Mockito").thenReturn("is neat!!");
-		String result = i.next() + " " + i.next();
-		assertEquals("Mockito is neat!!", result);
-	}
+    /**
+     * Test 2. More than one return value. Demonstrates the return of multiple
+     * values
+     */
+    @SuppressWarnings("rawtypes")
+    @Test
+    public void test2_MoreThanOneReturnValue() {
+	final Iterator i = mock(Iterator.class);
+	when(i.next()).thenReturn("Mockito").thenReturn("is neat!!");
+	String result = i.next() + " " + i.next();
+	assertEquals("Mockito is neat!!", result);
+    }
 
-	/**
-	 * Test return value dependent on method parameter.
-	 */
-	@Test
-	public void testReturnValueDependentOnMethodParameter() {
-		Comparable c = mock(Comparable.class);
-		when(c.compareTo("Mockito")).thenReturn(1);
-		when(c.compareTo("Eclipse")).thenReturn(2);
-		// assert
-		assertEquals(1, c.compareTo("Mockito"));
-	}
+    /**
+     * Test 3. Return value dependent on method parameter.
+     */
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    @Test
+    public void test3_ReturnValueDependentOnMethodParameter() {
+	Comparable c = mock(Comparable.class);
+	when(c.compareTo("Mockito")).thenReturn(1);
+	when(c.compareTo("Eclipse")).thenReturn(2);
+	// assert
+	assertEquals(1, c.compareTo("Mockito"));
+    }
 
-	/**
-	 * Test return value in dependent on method parameter.
-	 */
-	@Test
-	public void testReturnValueInDependentOnMethodParameter() {
-		Comparable c = mock(Comparable.class);
-		when(c.compareTo(anyInt())).thenReturn(-1);
-		assertEquals(-1, c.compareTo(9));
-	}
+    /**
+     * Test 4. Return value in dependent on method parameter.
+     */
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    @Test
+    public void test4_ReturnValueInDependentOnMethodParameter() {
+	final Comparable c = mock(Comparable.class);
+	when(c.compareTo(anyInt())).thenReturn(-1);
+	assertEquals(-1, c.compareTo(9));
+    }
 
-	@Test
-	public void testVerify() {
-		// create and configure mock
-		TestService test = Mockito.mock(TestService.class);
-		when(test.getUniqueId()).thenReturn(43);
+    /**
+     * Test 5. Configuring service-mock and testing/verifying it
+     */
+    @Test
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    public void test5_VerifyMockedService() {
 
-		// call method testing on the mock with parameter 12
-		test.testing(12);
-		test.getUniqueId();
-		test.getUniqueId();
-		test.someMethod("Hello World");
-		test.someMethod("called at least once");
-		test.someMethod("called at least twice");
-		test.someMethod("called five times");
-		test.someMethod("called at most 3 times");
+	// -------------------------------------------------------------------
+	// Creation and configuration of the service-mock
+	final TestService test = Mockito.mock(TestService.class);
+	when(test.getUniqueId()).thenReturn(43);
+	// -------------------------------------------------------------------
 
-		// now check if method testing was called with the parameter 12
-		verify(test).testing(Matchers.eq(12));
+	// -------------------------------------------------------------------
+	// Test-sequence of method calls in the mocked-service, 
+	// e.g. testing-method called on the mock with parameter 12
+	// -------------------------------------------------------------------
+	test.testing(12);
+	test.getUniqueId();
+	test.getUniqueId();
+	test.someMethod("Hello World");
+	test.someMethod("called at least once");
+	test.someMethod("called at least twice");
+	test.someMethod("called five times");
+	test.someMethod("called at most 3 times");
+	
+	//
+	// Verification
+	//
 
-		// was the method called twice?
-		verify(test, times(2)).getUniqueId();
+	// check if testing-method was called with the parameter 12
+	verify(test).testing(Matchers.eq(12));
 
-		// other alternatives for verifiying the number of method calls for a
-		// method
-		verify(test, never()).someMethod("never called");
-		verify(test, atLeastOnce()).someMethod("called at least once");
-		
-		//	Will all fail because we didn't met the conditions.
-		verify(test, atLeast(2)).someMethod("called at least twice");	 
-		verify(test, times(5)).someMethod("called five times");
-		verify(test, atMost(3)).someMethod("called at most 3 times");
-	}
+	// was the getUniqueId-method called twice?
+	verify(test, times(2)).getUniqueId();
+
+	// other alternatives for verifying 
+	// the number of method calls for a method
+	verify(test, never()).someMethod("never called");
+	verify(test, atLeastOnce()).someMethod("called at least once");
+
+	// fails because we didn't met the conditions.
+	verify(test, atLeast(2)).someMethod("called at least twice");
+	verify(test, times(5)).someMethod("called five times");
+	verify(test, atMost(3)).someMethod("called at most 3 times");
+    }
 
 }
